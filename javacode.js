74d3c07533f837cbd00f1ea27d5e9f6afecfc8d0
ReidTree <rt.trierweiler@gmail.com>
2017-06-14 01:01:35 -0400

    adding Javascript portion

---
+++ javacode.js
@@ -0,0 +1,7 @@
+
+function originalValue()
+    {
+    document.getElementById("spruceText").style.backgroundColor = "#000000";
+    document.getElementById("spruceText").style.color = "#32cd32";
+    document.getElementById("spruceText").value = document.getElementById("spruceText").value.toUpperCase();
+    }
